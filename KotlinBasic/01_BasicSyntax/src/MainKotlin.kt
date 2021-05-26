5afd4c0039befae01c723ddc1e9e46b16fea193a
Marina <marinkatena@gmail.com>
2021-05-26 16:29:28 +0300

    Inital commit

--- KotlinBasic/01_BasicSyntax/src/MainKotlin.kt
+++ KotlinBasic/01_BasicSyntax/src/MainKotlin.kt
@@ -1,7 +1,27 @@
 fun main(){
-    val firstName: String = "Marina"
+    val firstName: String = "Марина"
     println(firstName)
-    var lastName: String = "Ogar"
+    val lastName: String = "Огарь"
     println(lastName)
+
+    val height: Int = 156
+    val height2: String = "см"
+    val myHeight: String = "Мой рост: $height $height2"
+    println(myHeight)
+
+    val weight = 48.5F
+    val weight2: String = "кг"
+    val myWeight: String = "Мой вес: $weight $weight2"
+    println(myWeight)
+
+    val isChild: Any = if((height > 150) && (weight>40))
+        "Взрослый" else
+        "Ребенок"
+    val me: String = "Мой статус: $isChild"
+    println(me)
+
+val info: String = "Информация обо мне: $firstName $lastName; $myHeight; $myWeight; $me. "
+    println(info)
+
 
 }
