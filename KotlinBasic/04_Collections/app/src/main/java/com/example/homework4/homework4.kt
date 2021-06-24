d14a01b39b6cf3ef499e5403bf404a94b102a3ee
Marina <marinkatena@gmail.com>
2021-06-24 20:33:35 +0300

    homework4.3

--- KotlinBasic/04_Collections/app/src/main/java/com/example/homework4/homework4.kt
+++ KotlinBasic/04_Collections/app/src/main/java/com/example/homework4/homework4.kt
@@ -6,15 +6,25 @@ fun main(){
     val s = numberTel(n)
     println("$s")
 
-    val filteredList = numberTel(n).filter {it.startsWith(prefix = "+7")}
+    val filteredList = s.filter {it.startsWith(prefix = "+7")}
     println(filteredList)
 
-    val listToSet = numberTel(n).toSet()
+    val listToSet = s.toSet()
     println("Количество уникальных номеров: ${listToSet.size}")
 
-    val lenghtSet= listToSet.sumBy { it.length }
-    println("Сумма: $lenghtSet")
+    val lengthSet= listToSet.sumBy { it.length }
+    println("Сумма: $lengthSet")
 
+    val phoneBook = mutableMapOf<String,String>()
+    for (t in s) {
+        print("Введите имя человека с номером телефона $t: ")
+        val p = (readLine()?.toString()?:0)
+        phoneBook[t]= p.toString()
+    }
+
+    for(entry in phoneBook) {
+        println("Человек: ${phoneBook[entry.key]}. Номер телефона: ${phoneBook[entry.value]}")
+    }
 }
 
 fun numberTel(n: Int): MutableList<String> {
@@ -23,17 +33,6 @@ fun numberTel(n: Int): MutableList<String> {
     for (i in 1..n) {
         print("Введите номер телефона: ")
         val t = readLine()?.toString()?.let { newNumber.add(it) }
-    }
-
-    val phoneBook = mutableMapOf<String,String>()
-    for (t in newNumber) {
-        print("Введите имя человека с номером телефона $t: ")
-        val p = (readLine()?.toString()?:0)
-        phoneBook[t]= p.toString()
-    }
-
-    for(entry in phoneBook) {
-        println("Человек: ${phoneBook[entry.key]}. Номер телефона: ${phoneBook[entry.value]}")
     }
     return newNumber
 }
