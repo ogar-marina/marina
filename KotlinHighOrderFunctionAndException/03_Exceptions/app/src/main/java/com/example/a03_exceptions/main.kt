7acccf0d850b10e16383099efe5e2267402795b5
Marina <marinkatena@gmail.com>
2021-07-30 12:22:46 +0300

    homework 8.7

---
+++ KotlinHighOrderFunctionAndException/03_Exceptions/app/src/main/java/com/example/a03_exceptions/main.kt
@@ -0,0 +1,23 @@
+package com.example.a03_exceptions
+
+fun main(){
+    val wheel = Wheel()
+
+    try{
+        wheel.setPressure(2.0)
+        //wheel.setPressure(6.0)
+        //wheel.setPressure(1.0)
+        //wheel.setPressure(-5.0)
+    }
+    catch (t: Wheel.IncorrectPressure){
+        println(t.message)
+    }
+
+    catch (t: Wheel.TooLowPressure){
+        println(t.message)
+    }
+
+    catch (t: Wheel.TooHighPressure){
+        println(t.message)
+    }
+}
