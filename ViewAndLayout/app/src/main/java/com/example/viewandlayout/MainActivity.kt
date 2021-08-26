f1db0b7de607e6fd9402a642a5110fe452648b04
Marina Ogar <marinkatena@gmail.com>
2021-08-26 22:04:56 +0300

    homework 10.8

--- ViewAndLayout/app/src/main/java/com/example/viewandlayout/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/example/viewandlayout/MainActivity.kt
@@ -1,71 +1,54 @@
 package com.example.viewandlayout
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.Gravity
-import android.view.View
-import android.view.View.VISIBLE
-import android.view.ViewGroup
 import android.widget.*
-import kotlin.random.Random
+import androidx.appcompat.app.AppCompatActivity
+import com.example.viewandlayout.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val loginButton = findViewById<Button>(R.id.LoginButton)
-
-
-        loginButton.setOnClickListener {
+        binding.LoginButton.setOnClickListener {
             login()
         }
-
     }
 
-
     private fun login() {
-
-        val loginButton = findViewById<Button>(R.id.LoginButton)
-        val container = findViewById<LinearLayout>(R.id.container)
-
-        loginButton.setOnClickListener {
+        binding.LoginButton.setOnClickListener {
             val progressBar = ProgressBar(this).apply {
                 layoutParams = LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT,
                 )
-               }
-            container.addView(progressBar)
-
-            progressBar.visibility = View.VISIBLE
+            }
+            binding.container.addView(progressBar)
             enable(false)
-
             Handler().postDelayed({
-                progressBar.visibility = View.GONE
+                binding.container.removeView(progressBar)
                 enable(true)
-
                 Toast.makeText(this, "Логин прошел успешно!", Toast.LENGTH_SHORT).show()
             }, 2000)
-
-
-         }
-
-
+        }
     }
-
 
     private fun enable(b: Boolean) {
-        val loginButton = findViewById<Button>(R.id.LoginButton)
-        val checkBoxExample = findViewById<CheckBox>(R.id.checkboxExample)
-        val textEmail = findViewById<EditText>(R.id.textEmail)
-        val textPassword = findViewById<EditText>(R.id.textPassword)
 
-        loginButton.isEnabled = b
-        checkBoxExample.isEnabled = b
-        textEmail.isEnabled = b
-        textPassword.isEnabled = b
+        binding.LoginButton.isEnabled = b
+        binding.checkboxExample.isEnabled = b
+        binding.textEmail.isEnabled = b
+        binding.textPassword.isEnabled = b
+
     }
 }
+
+
+
+
