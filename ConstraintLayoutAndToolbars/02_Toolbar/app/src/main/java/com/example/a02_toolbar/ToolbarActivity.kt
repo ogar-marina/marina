715702bd826ba1d7a071d0ada9c52829039be104
Marina Ogar <marinkatena@gmail.com>
2021-08-28 19:59:48 +0300

    homework 11.8

---
+++ ConstraintLayoutAndToolbars/02_Toolbar/app/src/main/java/com/example/a02_toolbar/ToolbarActivity.kt
@@ -0,0 +1,64 @@
+package com.example.a02_toolbar
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.example.a02_toolbar.databinding.ActivityToolbarBinding
+
+class ToolbarActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityToolbarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityToolbarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initToolbar()
+    }
+
+    private fun toast(text:String){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initToolbar() {
+        binding.ToolBar.setNavigationOnClickListener {
+            toast("Navigation clicked")
+        }
+
+        binding.ToolBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_1 -> {
+                    toast("action 1 clicked")
+                    true
+                }
+                R.id.action_2 -> {
+                    toast("action 2 clicked")
+                    true
+                }
+                R.id.action_3 -> {
+                    toast("action 3 clicked")
+                    true
+                }
+                else -> false
+            }
+        }
+
+        val searchItem = binding.ToolBar.menu.findItem(R.id.action_search)
+        searchItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener{
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+               return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                return true
+            }
+
+        })
+
+    }
+
+
+}
