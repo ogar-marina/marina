7f372bd0b1f2967186faf53029bfc787e4c6bf4d
Marina <marinkatena@gmail.com>
2021-10-08 08:42:17 +0300

    homework 16.8

--- Lists_1/app/src/main/java/com/skillbox/lists1/DialogFragment.kt
+++ Lists_1/app/src/main/java/com/skillbox/lists1/DialogFragment.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 
-class DialogFragment: DialogFragment() {
+class DialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val newUser = arrayOf("User", "Developer")
         return AlertDialog.Builder(requireActivity())
             .setTitle("Добавить")
             .setView(R.layout.add_user_dialog)
-            .setPositiveButton("Ok", { _, _ ->  })
-            .setNegativeButton("Cancle", { _, _ ->  })
+            .setPositiveButton("Ok", { _, _ -> })
+            .setNegativeButton("Cancle", { _, _ -> })
             .setItems(newUser) { _, which -> }
             .create()
     }
--- Lists_1/app/src/main/java/com/skillbox/lists1/MainActivity.kt
+++ Lists_1/app/src/main/java/com/skillbox/lists1/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PersonListFragment())
                 .commit()
